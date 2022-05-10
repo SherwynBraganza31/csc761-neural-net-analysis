31a7a021b4af8e2d695d681bb8b2ab1d1ac70a82
sherwyn_b <sherwyn.braganza@mines.sdsmt.edu>
2022-05-10 11:38:00 -0600

    Changes to documentation

--- cnn.py
+++ cnn.py
@@ -4,26 +4,34 @@ from torch.autograd import Variable
 from torch import nn, optim
 from torchvision import transforms, datasets
 
-
-"""
-MNIST Dataset Dataloaders Class
-
-    Attributes
-    ----------
-    trainset : torch.datasets
-        torchvision datasets class representing the training set
-    testset : torch.datasets
-        torchvision datasets class representing the testing set
-    trainloader : torch.utils.data.DataLoader
-        torchvision datasets class representing the training set labels
-    testloader : torch.utils.data.DataLoader
-        torchvision datasets class representing the testing set labels
-    batch_size : int
-        the size of sample set to be loaded in when testing and training
-
-"""
 class DatasetsMNIST:
+    """
+    MNIST Dataset Dataloaders Class
+
+        Attributes
+        ----------
+        trainset : torch.datasets
+            torchvision datasets class representing the training set
+
+        testset : torch.datasets
+            torchvision datasets class representing the testing set
+
+        trainloader : torch.utils.data.DataLoader
+            torchvision datasets class representing the training set labels
+
+        testloader : torch.utils.data.DataLoader
+            torchvision datasets class representing the testing set labels
+
+        batch_size : int
+            the size of sample set to be loaded in when testing and training
+
+    """
+
     def __init__(self, batch_size=64):
+        """
+        :param batch_size: int
+            the size of the sample set to load in at a time
+        """
         self.trainset = datasets.MNIST(root='mnist_dataset', download=True, train=True, transform=transforms.ToTensor())
         self.testset = datasets.MNIST(root='mnist_dataset', download=True, train=False, transform=transforms.ToTensor())
         self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=batch_size, shuffle=True)
@@ -35,24 +43,31 @@ class DatasetsMNIST:
     #                                          transforms.Normalize((0.5,), (0.5,))])
     #     return self.transform
 
-"""
-Log Dictionary Class
-Creates a list of dictionaries that serves as a log file. 
-Checks for existing log files to append to or creates a new one
-if none exit
 
-    Attributes
-    ----------
-    log_dictionary: list
-        list of dictionaries acting as epoch logs
-    epoch_pad: int
-        the last read epoch # from the file
-    filename: string
-        the name of the old logfile
-
-"""
 class LogDict:
+    """
+    Log Dictionary Class
+    Creates a list of dictionaries that serves as a log file.
+    Checks for existing log files to append to or creates a new one
+    if none exit
+
+        Attributes
+        ----------
+        dictionary: list
+            list object of dictionaries acting as epoch logs
+
+        epoch_pad: int
+            the last read epoch # from the file
+
+        filename: string
+            the name of the old logfile
+
+    """
     def __init__(self, filename: str):
+        """
+        :param filename: str
+            name of existing logfile
+        """
         self.dictionary = []
         self.epoch_pad = 0
         try:
@@ -66,54 +81,65 @@ class LogDict:
             print("No Log file found")
 
     def save_log_file(self, filename='cnn.log'):
+        """
+        Save log to a file
+        :param filename: string
+            name of the file to save to
+        :return: None
+        """
         with open(filename, 'a', encoding='utf-8') as f:
             for x in self.dictionary:
                 f.write(str(x[0]) + "," + str(x[1]) + "\n")
             f.close()
+        pass
 
-"""
-CNN Model Definition
-Inherits from superclass nn.Module.
-4 Layers -> 2 Convolutional
 
-    Attributes
-    ----------
-    conv1: torch.nn.Sequential
-        First Convolutional Layer. 1 input (unchangeable), 16 outputs.
-        
-    conv2: torch.nn.Sequential
-        Second Convolutional Layer. 16 inputs, 32 outputs. Changeable with changes made to 1st layer
-        
-    output: torch.nn.Sequential
-        Output Layer. Implements a softmax ish output.
-        
-    optimizer: torch.optim
-        Optimizer module
-        
-    criterion: torch.nn
-        Loss method object
-        
-    Methods
-    -------
-    forward(x)
-        inherits from parent class. Links all NN modules created
-        
-    load_model_from_file(filename)
-        loads a torch.state_dict from the saved model specified by
-        filename and updates the model params
-        
-    config_optimizer(learning_rate)
-        configures the optimizer object model
-        
-    config_loss_function()
-        configures loss function parameters
-        
-    save_state(filename)
-        saves the torch.state_dict corresponding to the models current
-        params      
-"""
 class ModelCNN(nn.Module):
+    """
+    CNN Model Definition
+    Inherits from superclass nn.Module.
+    4 Layers -> 2 Convolutional
+
+        Attributes
+        ----------
+        conv1: torch.nn.Sequential
+            First Convolutional Layer. 1 input (unchangeable), 16 outputs.
+
+        conv2: torch.nn.Sequential
+            Second Convolutional Layer. 16 inputs, 32 outputs. Changeable with changes made to 1st layer
+
+        output: torch.nn.Sequential
+            Output Layer. Implements a softmax ish output.
+
+        optimizer: torch.optim
+            Optimizer module
+
+        criterion: torch.nn
+            Loss method object
+
+        Methods
+        -------
+        forward(x)
+            inherits from parent class. Links all NN modules created
+
+        load_model_from_file(filename)
+            loads a torch.state_dict from the saved model specified by
+            filename and updates the model params
+
+        config_optimizer(learning_rate)
+            configures the optimizer object model
+
+        config_loss_function()
+            configures loss function parameters
+
+        save_state(filename)
+            saves the torch.state_dict corresponding to the models current
+            params
+    """
     def __init__(self):
+        """
+        Initializes attributes of parent class and creates optimizer and loss calculator objects
+        """
         super().__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(
@@ -147,6 +173,15 @@ class ModelCNN(nn.Module):
         pass
 
     def forward(self, x):
+        """
+        inherited from parent class.
+        :param x: nn.Module
+            current state of NN Module
+        :return: output:
+            calculated output of the NN
+        :return x: nn.Module
+            state of the neural network (returned for visualization)
+        """
         x = self.conv1(x)
         x = self.conv2(x)
         # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
@@ -154,7 +189,16 @@ class ModelCNN(nn.Module):
         output = self.out(x)
         return output, x  # return x for visualization
 
-    def load_model_from_file(self, filename):
+    def load_model_from_file(self, filename:str):
+        """
+        Loads an a torch.state_dict from file <filename>. If no file was found,
+        it initializes the optimizer and loss_function and returns the original
+        model.
+        :param filename: str
+            name of the file to open from
+        :return: self: ModelCNN
+            the ModelCNN recovered from the file
+        """
         try:
             self.load_state_dict(torch.load(filename))
             self.eval()
@@ -166,15 +210,34 @@ class ModelCNN(nn.Module):
         return self
 
     def config_optimizer(self, learning_rate=0.001):
+        """
+        Configures an optimizer or learner for the model.
+        Defaults to Adam Optimizer with learning rate of 0.001
+        :param learning_rate: float
+            learning rate of the model
+        :return: self.optimizer
+            the optimizer object created
+        """
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
         return self.optimizer
 
     def config_loss_function(self):
+        """
+        Configures a loss function for the model.
+        Defaults to CrossEntropyLoss.
+        :return: self.criterion
+            the loss function object created.
+        """
         self.criterion = nn.CrossEntropyLoss()
         return self.criterion
 
     def save_state(self, filename='cnn.sav'):
-        torch.save(self.state_dict(), "cnn.sav")
+        """
+        :param filename: str
+            The name of the file to save.
+        :return: None
+        """
+        torch.save(self.state_dict(), filename)
 
 
 cnn = ModelCNN()
